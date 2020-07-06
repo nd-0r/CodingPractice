b6b869f789bdd28f710b536d2c70fe2e572d8720
Andrew Orals <andreworals@icloud.com>
2020-07-05 23:32:09 -0500

    finished another challenge

--- ConsChallenge.java
+++ ConsChallenge.java
@@ -4,34 +4,45 @@ import java.util.*;
     Create a function that determines whether elements in 
     an array can be re-arranged to form a consecutive list 
     of numbers where each number appears exactly once.
-*/
 
-public static class ConsChallenge {
-
-    private static int count = 1;
-
-    private static boolean find(int start, int[] arr1) {
-        if(count == arr1.length) { return true; }
+if(count == arr1.length) { return true; }
         for(int i = 0; i < arr1.length; i ++) {
             if(arr1[i] == start + 1) {
-                count ++;
                 find(start + 1, arr1);
             }
         }
-        count ++;
         return false;
-    }
+
+*/
+
+public class ConsChallenge {
+
     public static boolean cons(int[] arr) {
-        count = 1;
         int min = Integer.MAX_VALUE;
+        int count = 0;
+        boolean found = false;
+
         for(int i = 0; i < arr.length; i ++) {
             if(arr[i] < min) { min = arr[i]; }
         }
-        return find(min, arr0);
+        
+        while(count < arr.length - 1) {
+            found = false;
+            for(int i = 0; i < arr.length; i ++) {
+                if(arr[i] == min + 1) {
+                    min = min + 1;
+                    i = arr.length;
+                    found = true;
+                }
+            }
+            if( ! found) { return false; }
+            count ++;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        int[] in = {3, 5, 4, 1, 2};
+        int[] in = {3, 6, 4, 1, 2};
         System.out.println(cons(in));
     }
 
