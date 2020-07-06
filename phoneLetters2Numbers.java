f8b5fc5fefd1314a7333324314d8c1f36833ecd2
Andrew Orals <andreworals@icloud.com>
2020-07-06 00:08:43 -0500

    Started the phone number challenge

---
+++ phoneLetters2Numbers.java
@@ -0,0 +1,29 @@
+import java.util.Dictionary;
+
+
+
+class TextToNum {
+
+    public String textToNum(String in) {
+
+        HashMap<String, Integer> letterCorrelations = new HashMap<String, String>();
+        letterCorrelations.put("ABC", "2");
+        letterCorrelations.put("DEF", "3");
+        letterCorrelations.put("GHI", "4");
+        letterCorrelations.put("JKL", "5");
+        letterCorrelations.put("MNO", "6");
+        letterCorrelations.put("PQRS", "7");
+        letterCorrelations.put("TUV", "8");
+        letterCorrelations.put("WXYZ", "9");
+
+        String out = "";
+        for(int i = 0; i < in.length(); i ++) {
+            if(Character.isLetter(in.charAt(i))) {
+                out += letterCorrelations.get()
+            } else {
+                out += in.charAt(i) + "";
+            }
+        }
+    }
+
+}
