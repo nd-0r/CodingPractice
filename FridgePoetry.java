fd114a6c888f3b6c2e5eec8375aba6dab18279b1
Andrew Orals <andreworals@icloud.com>
2020-07-11 12:17:18 -0500

    did fridge poetry and learned about sorting

--- FridgePoetry.java
+++ FridgePoetry.java
@@ -12,8 +12,56 @@
 
 public class FridgePoetry {
 
+    public static String removeSpaces(String in) {
+
+        if(! in.contains(" ")) {
+            return in;
+        }
+        
+        return removeSpaces(in.substring(0, in.indexOf(" ")) + in.substring(in.indexOf(" ") + 1));
+
+    }
+
+    public static String lexicoSort(String str) {
+
+        char[] in = str.toCharArray();
+
+        char tmp;
+
+        for(int i = 0; i < in.length; i ++) {
+            for(int j = i; j > 0; j --) {
+                if(in[j - 1] > in[j]) {
+                    tmp = in[j - 1];
+                    in[j - 1] = in[j];
+                    in[j] = tmp;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return new String(in);
+
+    }
+
     public static boolean canBuild(String s1, String s2) {
-		
+        
+        String s1new = lexicoSort(removeSpaces(s1));
+        String s2new = lexicoSort(removeSpaces(s2));
+
+        System.out.println(s1new);
+        System.out.println(s2new);
+
+        if(s1new.equals(s2new)) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canBuild("i am bob", "bobami"));
     }
 
 }
