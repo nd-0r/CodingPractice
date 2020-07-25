d4cee7d5140b420537cf6deb2b2b81515ed925a2
Andrew Orals <andreworals@icloud.com>
2020-07-25 00:04:42 -0500

    another commit

--- multiplicativePersistence.java
+++ multiplicativePersistence.java
@@ -1,5 +1,6 @@
 public class MultiplicativePersistence {
-    public static int bugger(int num) {
+    
+    public static int multiplicativePersistence(int num) {
 
         int test = 1;
         boolean cont = true;
@@ -27,9 +28,38 @@ public class MultiplicativePersistence {
 
     }
 
-    public static void main(String[] args) {
+    public static int additivePersistence(int num) {
 
-        System.out.println(bugger(39));
+        int test = 0;
+        boolean cont = true;
+        int count = 1;
+        
+        if(num < 10) { return 0; }
+
+        do {
+        	
+        	test = 0;
+            while(num > 0) {
+                test += (num % 10);
+                num /= 10;
+            }
+            if(test < 10) {
+                cont = false;
+            } else {
+            	count++;
+                num = test;
+            }
+
+        } while(cont);
+        
+        return count;
+
+    }
+
+    public static void main(final String[] args) {
+
+        System.out.println(multiplicativePersistence(5789));
+        System.out.println(additivePersistence(5789));
 
     }
 }
